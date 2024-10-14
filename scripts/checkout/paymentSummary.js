f1efd8b91d54c17fe946c5268489921ffabe890e
kingjeongkong <wjdqls9223@gmail.com>
2024-10-14 19:14:36 +0900

    Order 생성

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -56,4 +57,26 @@ export function renderPaymentSummary() {
 
   document.querySelector('.payment-summary')
     .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.place-order-button')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+  
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.log('Unexpected error. Try again later')
+      }
+
+      window.location.href = 'orders.html';
+    });
 }
